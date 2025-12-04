224b0de4ea03c6916c7fa804f40fb67454a8cd28
HarpreetSingh2005 <harpreetsinghgnps@gmail.com>
2025-12-04 16:07:23 +0530

    first commit

---
+++ src/constants/index.js
@@ -0,0 +1,9 @@
+// Can be accessed by just using ../constants, and not need to specifc the whole path, because its index
+export const navLinks = [
+    { label: 'Store'},
+    { label: 'Mac'},
+    { label: 'iPhone'},
+    { label: 'Watch'},
+    { label: 'Vision'},
+    { label: 'Airpods'},
+]
