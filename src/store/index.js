0496b1242a83e5c2b0fec836987b8f38795daac1
HarpreetSingh2005 <harpreetsinghgnps@gmail.com>
2025-12-13 22:41:44 +0530

    Features Laptop Scroll Section

--- src/store/index.js
+++ src/store/index.js
@@ -1,3 +1,4 @@
+import { texture } from "three/tsl";
 import { create } from "zustand";
 
 const useMacbookStore = create((set) => ({
@@ -7,10 +8,14 @@ const useMacbookStore = create((set) => ({
     scale: 0.08,
     setScale: (scale) => set({ scale }),
 
+    texture: '/videos/feature-1.mp4',
+    setTexture: (texture) => set({ texture }),
+
     reset: () => set({
         color: '#2e2c2e',
-        scale: 0.08
+        scale: 0.08,
+        texture: '/videos/feature-1.mp4',
     }),
 }))
 
-export default useMacbookStore
+export default useMacbookStore;
